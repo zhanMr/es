b6edaccd9bb072fb03af304326b0a405d7233573
zhan <zqx@zhandeMacBook-Pro.local>
2020-12-27 19:53:13 +0800

    基础文件#

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getVersion = void 0;
+var a = 100;
+function getVersion(version) {
+    if (version === void 0) { version = "1.0.0"; }
+    return a + version;
+}
+exports.getVersion = getVersion;
+console.log(document);
+//# sourceMappingURL=index.js.map
