c0bb13beea784d36f72d51fb67314300ccc20fce
zhan <zqx@zhandeMacBook-Pro.local>
2021-01-01 21:55:50 +0800

    基础工程

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // 返回当前版本号
-       const a = 100;
-export function getVersion(version = "1.0.0"): string{
-    return a + version;
+   const a = 100;
+export function getVersion(version = '1.0.0'): string {
+  return a + version;
 }
-console.log(document)
+console.log(document);
 //@typescript-eslint/eslint-plugin@latest @typescript-eslint/parser@latest
